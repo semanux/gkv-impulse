eb169379a928e06a05b4297f48d1401ceaf127c2
raphaelmenges <unrealer2@yahoo.de>
2022-10-08 14:22:59 +0200

    Add QR code with link to own URL.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+import QrcodeVue from "qrcode.vue";
+
+const url = computed(() => window.location.href);
 
 </script>
 
 <template>
   <h1>Semanux @ GKV im:pulse</h1>
   <span>Hello World</span>
+  <qrcode-vue
+    :value="url"
+    :render-as="'svg'"
+    :level="'L'"
+  />
 </template>
 
 <style>
-
 </style>
